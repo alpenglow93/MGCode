a1d9648771516c3cbec47202a950edeaa492d422
alpenglow93 <dktmzptm@gmail.com>
2016-11-30 21:19:16 +0900

    지현 - Pattern

---
+++ 지현 - Pattern/StatePattern/StatePattern/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//상태패턴 : 객체의 상태 자체를 바꾸는 패턴
+class Program
+{
+    static void Main(string[] args)
+    {
+        //객체에 A 클래스 데이터를 대입
+        Context c = new Context(new ConcreteStateA());
+
+        c.Request();
+        c.Request();
+        c.Request();
+        c.Request();
+    }
+}
+
+//추상클래스
+abstract class State
+{
+    public abstract void Handle(Context _context);
+}
+
+//추상클래스를 상속받는 클래스A
+class ConcreteStateA : State
+{
+    public override void Handle(Context _context)
+    {
+        //B 클래스의 데이터 대입
+        _context.State = new ConcreteStateB();
+    }
+}
+
+//추상클래스를 상속받는 클래스B
+class ConcreteStateB : State
+{
+    public override void Handle(Context _context)
+    {
+        //A 클래스의 데이터 대입
+        _context.State = new ConcreteStateA();
+    }
+}
+
+class Context
+{
+    //State 클래스 변수
+    private State state;
+
+    public Context(State _state)
+    {
+        state = _state;
+    }
+
+    public State State
+    {
+        get { return state; }
+        set
+        {
+            state = value;
+            //어느 데이터를 담은 객체인지 알려줄 코드
+            Console.WriteLine("State: " + state.GetType().Name);
+        }
+    }
+
+    public void Request()
+    {
+        state.Handle(this);
+    }
+}
---
+++ 지현 - Pattern/StrategyPattern/StrategyPattern/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//전략패턴 : 동일 계열의 알고리즘군을 정의하고, 각 알고리즘을 캡슐화하는 패턴
+class Program
+{
+    static void Main(string[] args)
+    {
+        Context context;
+
+        context = new Context(new ConcreteStrategyA());
+        context.ContextInterface();
+
+        context = new Context(new ConcreteStrategyB());
+        context.ContextInterface();
+
+        context = new Context(new ConcreteStrategyC());
+        context.ContextInterface();
+
+        //Console.ReadKey();
+    }
+}
+
+//추상클래스
+abstract class Strategy
+{
+    public abstract void AlgorithmInterface();
+}
+
+//추상클래스를 상속받는 클래스 A
+class ConcreteStrategyA : Strategy
+{
+    public override void AlgorithmInterface()
+    {
+        Console.WriteLine("Call ConcreteStrategyA.AlgorithmInterface()");
+    }
+}
+
+//추상클래스를 상속받는 클래스 B
+class ConcreteStrategyB : Strategy
+{
+    public override void AlgorithmInterface()
+    {
+        Console.WriteLine("Call ConcreteStrategyB.AlgorithmInterface()");
+    }
+}
+
+//추상클래스를 상속받는 클래스 C
+class ConcreteStrategyC : Strategy
+{
+    public override void AlgorithmInterface()
+    {
+        Console.WriteLine("Call ConcreteStrategyC.AlgorithmInterface()");
+    }
+}
+//각각 해당 클래스의 메서드를 호출했다는 메시지를 출력하는 코드가 들어있음
+
+//Context 클래스
+class Context
+{
+    private Strategy strategy;
+
+    public Context(Strategy _strategy)
+    {
+        strategy = _strategy;
+    }
+
+    public void ContextInterface()
+    {
+        strategy.AlgorithmInterface();
+    }
+}
